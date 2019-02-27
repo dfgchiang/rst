4b1a595d669b86aa6a1d02977e95f9e28bb998bf
Dean Chiang <dean.chiang@wildlife.ca.gov>
2019-02-26 17:34:06 -0800

    deanlab mod

--- rlab/h3.R
+++ rlab/h3.R
@@ -2,7 +2,13 @@
 # dchiang 20190222
 
 # 1(a)
+library(readxl)
+doefund <- read_excel("C:/Users/dchiang/Downloads/stat128/deptOfEducationFundingbyState.xlsx", 
+                      range = "a7:f66")
+View(doefund)
+# 4thcol=amtchanged; 4thcol=pctchanged
 # 1(b)
 ?state.x77
 View(state.x77)
-?tapply
+?merge
+#?tapply
